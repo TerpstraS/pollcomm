375486eb491dfe1e52ab57907b7bde4a7f94168f
TerpstraS <70644294+TerpstraS@users.noreply.github.com>
2023-11-17 10:21:03 +0100

    Removed old imports

--- pollcomm/__init__.py
+++ pollcomm/__init__.py
@@ -1,7 +1,5 @@
 from .adaptive_model import *
-from .adaptive_resource_model import *
 from .base_model import *
 from .networknw import *
 from .pollcomm_class import *
 from .stats import *
-from .valdovinos import *
